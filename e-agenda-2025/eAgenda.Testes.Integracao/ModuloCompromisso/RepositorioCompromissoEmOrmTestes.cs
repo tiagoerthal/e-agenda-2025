b567aa9b7081a4339d00cf83f5b3b4232a1a46c4
Tiago <tiagoeweber@gmail.com>
2025-10-27 18:55:39 -0300

    Adicionados testes restantes para RepositorioCompromissoEmOrm

--- e-agenda-2025/eAgenda.Testes.Integracao/ModuloCompromisso/RepositorioCompromissoEmOrmTestes.cs
+++ e-agenda-2025/eAgenda.Testes.Integracao/ModuloCompromisso/RepositorioCompromissoEmOrmTestes.cs
@@ -69,5 +69,83 @@ namespace eAgenda.Testes.Integracao.ModuloCompromisso
 
             Assert.AreEqual(compromisso, compromissoSelecionado);
         }
+        [TestMethod]
+        public void Deve_EditarRegistro_ComSucesso()
+        {
+            // Arrange - Arranjo
+            Contato contato = Builder<Contato>
+                .CreateNew()
+                .Build();
+
+            TimeSpan horaInicio = DateTime.Now.TimeOfDay;
+            TimeSpan horaTermino = horaInicio.Add(TimeSpan.FromHours(2));
+
+            Compromisso compromisso = new Compromisso(
+                "Reunião",
+                DateTime.Now,
+                horaInicio,
+                horaTermino,
+                TipoCompromisso.Remoto,
+                "discord.com",
+                null,
+                contato
+            );
+
+            repositorioCompromisso?.CadastrarRegistro(compromisso);
+
+            Compromisso compromissoEditado = new Compromisso(
+                "Reunião Presencial",
+                DateTime.Now,
+                horaInicio,
+                horaTermino,
+                TipoCompromisso.Presencial,
+                null,
+                "MidiLages",
+                contato
+            );
+
+            // Act - Ação
+            bool? registroEditado = repositorioCompromisso?.EditarRegistro(compromisso.Id, compromissoEditado);
+
+            // Assert - Asserção
+            Compromisso? compromissoSelecionado = repositorioCompromisso?.SelecionarRegistroPorId(compromisso.Id);
+
+            Assert.IsTrue(registroEditado);
+            Assert.AreEqual(compromisso, compromissoSelecionado);
+        }
+
+        [TestMethod]
+        public void Deve_ExcluirRegistro_ComSucesso()
+        {
+            // Arrange - Arranjo
+            Contato contato = Builder<Contato>
+                .CreateNew()
+                .Build();
+
+            TimeSpan horaInicio = DateTime.Now.TimeOfDay;
+            TimeSpan horaTermino = horaInicio.Add(TimeSpan.FromHours(2));
+
+            Compromisso compromisso = new Compromisso(
+                "Reunião",
+                DateTime.Now,
+                horaInicio,
+                horaTermino,
+                TipoCompromisso.Remoto,
+                "discord.com",
+                null,
+                contato
+            );
+
+            repositorioCompromisso?.CadastrarRegistro(compromisso);
+
+            // Act - Ação
+            bool? registroExcluido = repositorioCompromisso?.ExcluirRegistro(compromisso.Id);
+
+            // Assert - Asserção
+            Compromisso? compromissoSelecionado = repositorioCompromisso?.SelecionarRegistroPorId(compromisso.Id);
+
+            Assert.IsTrue(registroExcluido);
+            Assert.IsNull(compromissoSelecionado);
+        }
     }
 }
