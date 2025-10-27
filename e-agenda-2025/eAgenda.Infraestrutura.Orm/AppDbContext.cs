ff3c8bb3ea87249f5df7beb7f2c62273712d81d9
Tiago <tiagoeweber@gmail.com>
2025-10-27 16:56:09 -0300

    Iniciada implementação de Testes de Integração de Contato

--- e-agenda-2025/eAgenda.Infraestrutura.Orm/AppDbContext.cs
+++ e-agenda-2025/eAgenda.Infraestrutura.Orm/AppDbContext.cs
@@ -12,6 +12,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace eAgenda.Infraestrutura.Orm;
 
+public static class AppDbContextFactory
+{
+    public static AppDbContext CriarDbContext(string connectionString)
+    {
+        var builder = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer(connectionString, options => options.EnableRetryOnFailure(3));
+
+        return new AppDbContext(builder.Options);
+    }
+}
+
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Contato> Contatos { get; set; }
---
+++ e-agenda-2025/eAgenda.Testes.Integracao/ModuloContato/RepositorioContatoEmOrmTestes.cs
@@ -0,0 +1,52 @@
+﻿using eAgenda.Dominio.ModuloContato;
+using eAgenda.Infraestrutura.Orm;
+using eAgenda.Infraestrutura.Orm.ModuloContato;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAgenda.Testes.Integracao.ModuloContato
+{
+    [TestClass]
+    public sealed class RepositorioContatoEmOrmTestes
+    {
+        private static readonly AppDbContext dbContext = AppDbContextFactory.CriarDbContext("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=E-agendaDb;Integrated Security=True");
+        private static RepositorioContatoEmOrm repositorioContato = new RepositorioContatoEmOrm(dbContext);
+
+        [TestMethod]
+        public void Deve_CadastrarRegistro_ComSucesso()
+        {
+            Contato contato = new Contato(
+                "Juninho Testes 2",
+                "(49) 98533-3334",
+                "testes@academiadoprogramador.net",
+                "Academia do Programador",
+                "Testador"
+            );
+
+            repositorioContato.CadastrarRegistro(contato);
+
+            Contato? contatoSelecionado = repositorioContato.SelecionarRegistroPorId(contato.Id);
+
+            // Asserção
+            Assert.AreEqual(contato, contatoSelecionado);
+        }
+
+        [TestMethod]
+        public void Deve_RetornarNulo_Ao_SelecionarRegistroPoId_ComIdErrado()
+        {
+            Contato contato = new Contato(
+                "Juninho Testes 2",
+                "(49) 98533-3334",
+                "testes@academiadoprogramador.net",
+                "Academia do Programador",
+                "Testador"
+            );
+
+            repositorioContato.CadastrarRegistro(contato);
+
+            Contato? contatoSelecionado = repositorioContato.SelecionarRegistroPorId(Guid.NewGuid());
+
+            // Asserção
+            Assert.AreNotEqual(contato, contatoSelecionado);
+        }
+    }
+}
