cdc3fc3c6addb27b31495574d9a69d554387e855
Tiago <tiagoeweber@gmail.com>
2025-10-27 17:05:42 -0300

    Implementados testes de Edição e Exclusão de Contatos

--- e-agenda-2025/eAgenda.Testes.Integracao/ModuloContato/RepositorioContatoEmOrmTestes.cs
+++ e-agenda-2025/eAgenda.Testes.Integracao/ModuloContato/RepositorioContatoEmOrmTestes.cs
@@ -48,5 +48,59 @@ namespace eAgenda.Testes.Integracao.ModuloContato
             // Asserção
             Assert.AreNotEqual(contato, contatoSelecionado);
         }
+        [TestMethod]
+        public void Deve_EditarRegistro_ComSucesso()
+        {
+            // Padrão AAA
+            // Arranjo
+            Contato contatoOriginal = new Contato(
+                "Juninho Testes",
+                "(49) 98533-3334",
+                "testes@academiadoprogramador.net",
+                "Academia do Programador",
+                "Testador"
+            );
+
+            repositorioContato.CadastrarRegistro(contatoOriginal);
+
+            Contato contatoEditado = new Contato(
+                "Pedrinho do Código",
+                "(49) 99452-5234",
+                "pedrinho_codigos@academiadoprogramador.net",
+                "Academia do Programador",
+                "Desenvolvedor"
+            );
+
+            // Ação
+            repositorioContato.EditarRegistro(contatoOriginal.Id, contatoEditado);
+
+            // Asserção
+            Contato? contatoSelecionado = repositorioContato.SelecionarRegistroPorId(contatoOriginal.Id);
+
+            Assert.AreEqual(contatoOriginal, contatoSelecionado);
+        }
+
+        [TestMethod]
+        public void Deve_ExcluirRegistro_ComSucesso()
+        {
+            // Arranjo
+            Contato contatoOriginal = new Contato(
+                "Juninho Testes",
+                "(49) 98533-3334",
+                "testes@academiadoprogramador.net",
+                "Academia do Programador",
+                "Testador"
+            );
+
+            repositorioContato.CadastrarRegistro(contatoOriginal);
+
+            // Ação
+            repositorioContato.ExcluirRegistro(contatoOriginal.Id);
+
+            // Asserção
+            Contato? contatoSelecionado = repositorioContato.SelecionarRegistroPorId(contatoOriginal.Id);
+
+            Assert.IsNull(contatoSelecionado);
+        }
     }
 }
