a7d72e458dc13e9e861157a583608b4eea2dda48
Tiago <tiagoeweber@gmail.com>
2025-11-03 18:01:09 -0300

    Implementados testes de integração de Categoria

---
+++ e-agenda-2025/eAgenda.Testes.Integracao/ModuloCategoria/RepositorioCategoriaEmOrmTestes.cs
@@ -0,0 +1,78 @@
+﻿
+using eAgenda.Dominio.ModuloCategoria;
+using eAgenda.Testes.Integracao.Compartilhado;
+
+namespace eAgenda.Testes.Integracao.ModuloCategoria;
+
+[TestClass]
+[TestCategory("Testes de Integração de Categoria")]
+public class RepositorioCategoriaEmOrmTestes : TestFixture
+{
+    [TestMethod]
+    public void Deve_CadastrarRegistro_ComSucesso()
+    {
+        // Arranjo
+        Categoria categoria = new Categoria("Mercado");
+
+        // Ação
+        repositorioCategoria?.CadastrarRegistro(categoria);
+
+        // Asserção
+        Categoria? categoriaSelecionada = repositorioCategoria?.SelecionarRegistroPorId(categoria.Id);
+
+        Assert.AreEqual(categoria, categoriaSelecionada);
+    }
+
+    [TestMethod]
+    public void Deve_RetornarNulo_Ao_SelecionarRegistroPorId_ComIdErrado()
+    {
+        // Arranjo
+        Categoria categoria = new Categoria("Mercado");
+
+        repositorioCategoria?.CadastrarRegistro(categoria);
+
+        // Ação
+        Categoria? categoriaSelecionada = repositorioCategoria?.SelecionarRegistroPorId(Guid.NewGuid());
+
+        // Asserção
+        Assert.AreNotEqual(categoria, categoriaSelecionada);
+    }
+
+    [TestMethod]
+    public void Deve_EditarRegistro_ComSucesso()
+    {
+        // Arranjo
+        Categoria categoriaOriginal = new Categoria("Mercado");
+
+        repositorioCategoria?.CadastrarRegistro(categoriaOriginal);
+
+        Categoria categoriaEditada = new Categoria("Lazer");
+
+        // Ação
+        bool? registroEditado = repositorioCategoria?.EditarRegistro(categoriaOriginal.Id, categoriaEditada);
+
+        // Asserção
+        Categoria? categoriaSelecionado = repositorioCategoria?.SelecionarRegistroPorId(categoriaOriginal.Id);
+
+        Assert.IsTrue(registroEditado);
+        Assert.AreEqual(categoriaOriginal, categoriaSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_ExcluirRegistro_ComSucesso()
+    {
+        // Arranjo
+        Categoria categoria = new Categoria("Mercado");
+
+        repositorioCategoria?.CadastrarRegistro(categoria);
+
+        // Ação
+        bool? registroExcluido = repositorioCategoria?.ExcluirRegistro(categoria.Id);
+
+        // Asserção
+        Categoria? categoriaSelecionada = repositorioCategoria?.SelecionarRegistroPorId(categoria.Id);
+
+        Assert.IsTrue(registroExcluido);
+        Assert.IsNull(categoriaSelecionada);
+    }
+}
